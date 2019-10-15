38b322b224163a4ec9806324013237b9b06189cc
Xvezda <xvezda@naver.com>
2019-10-16 04:04:34 +0900

    Deleted files

--- .travis/script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -e
-
-./install --setup all
-
--- src/automator/StripEXIF.sh
+++
@@ -1,5 +0,0 @@
-for file
-do
-	/usr/local/bin/mogrify -strip "$file"
-done
-osascript -e 'display alert "Stripping EXIF finished!"'
